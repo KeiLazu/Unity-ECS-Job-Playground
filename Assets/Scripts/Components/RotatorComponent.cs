83181b39354475c20a8a238ace3ae7c13e71e652
Kennix Lazuardi <kennixlazuardi@gmail.com>
2018-06-11 10:00:26 +0700

    initial commit

---
+++ Assets/Scripts/Components/RotatorComponent.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class RotatorComponent : MonoBehaviour
+{
+    public float speed;
+}
---
+++ Assets/Scripts/Systems/RotatorSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class RotatorSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        foreach (var item in GetEntities<Components>())
+        {
+            float deltaTime = Time.deltaTime;
+
+            item.transform.Rotate(0f, item.rotator.speed * deltaTime, 0f);
+        }
+    }
+
+    private struct Components
+    {
+        public RotatorComponent rotator;
+        public Transform transform;
+    }
+}
